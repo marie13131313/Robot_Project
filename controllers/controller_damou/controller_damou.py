88131601c640108cd732ec598ee370a670cee3a8
tatayoyoh <tatayoyoh@mymail>
2024-02-07 10:39:11 +0100

    add compass + print position

--- controllers/controller_damou/controller_damou.py
+++ controllers/controller_damou/controller_damou.py
@@ -2,14 +2,14 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot ,Motor , DistanceSensor
+from controller import Robot ,Motor , DistanceSensor, Compass
 
 class Motors ():
 
-    def __init__(self,robot):
+    def __init__(self,robot:Robot):
         super().__init__()
-        self.lwheel : Motor = self.getDevice('left whell')
-        self.rwheel : Motor = self.getDevice('right wheel')
+        self.lwheel : Motor = robot.getDevice('left wheel')
+        self.rwheel : Motor = robot.getDevice('right wheel')
     
     def turn_left(self):
         self.lwheel.setPosition(-10.0)
@@ -23,30 +23,35 @@ class monRobot(Robot):
     
     def __init__(self):
         super().__init__()
-        self.motors=Motors(robot)
+        self.timestep = int(self.getBasicTimeStep())
+        self.motors=Motors(self)
 
-        self.ps0:DistanceSensor = self.getDevice('ps0')
-        self.ps7:DistanceSensor = self.getDevice('ps7')
-        self.ps0.enable()
-        self.ps7.enable()
+
+        # self.ps0:DistanceSensor = self.getDevice('ps0')
+        # self.ps7:DistanceSensor = self.getDevice('ps7')
+        # self.ps0.enable(self.timestep)
+        # self.ps7.enable(self.timestep)
+        self.compass:Compass = self.getDevice('compass')
+        self.compass.enable(self.timestep)
 
 
     def run(self):
         self.motors.strait()
-        print(self.ps0.getValue())
-        if self.ps0.getValue() >= 0.5 and self.ps7.getValue() >= 0.5:
-            self.motors.turn_left()
-        pass
+        # print(self.ps0.getValue())
+        # if self.ps0.getValue() >= 0.5 and self.ps7.getValue() >= 0.5:
+        #     self.motors.turn_left()
+        # pass
 
+        print(self.compass.getValues())
 
-
+    
 
 
 # create the Robot instance.
 robot = monRobot()
 
 # get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
@@ -56,7 +61,7 @@ timestep = int(robot.getBasicTimeStep())
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
+while robot.step(robot.timestep) != -1:
 
     robot.run() # on avance et on esquive les murs
 
@@ -72,3 +77,4 @@ while robot.step(timestep) != -1:
     pass
 
 # Enter here exit cleanup code.
+
